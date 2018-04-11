6e41de4004d05ae4e29141f9a5e8663f8e4bbd10
sklee <hadesleeq@gmail.com>
2018-04-11 10:17:52 +0900

    코틀린 프로그램 기본 구조

--- src/Hello.kt
+++ src/Hello.kt
@@ -1,3 +1,13 @@
 fun main(args: Array<String>) {
-    println("Hello, Wolrd Kotlin~")
+    println(makeMessage1(1))
+    println(makeMessage1(2))
+    println(makeMessage2(1))
+    println(makeMessage2(2))
 }
+
+fun makeMessage2(msgType: Int) = if (msgType == 1) "날씨 좋죠?" else "참 맑군요"
+
+fun makeMessage1(msgType: Int): String {
+    return if (msgType == 1) "안녕하세요?" else "또 만났네요!!"
+}
+
