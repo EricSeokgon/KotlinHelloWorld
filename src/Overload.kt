993506e1fe7fa81bcc7ef584b90073321a10fa0f
sklee <hadesleeq@gmail.com>
2018-04-13 11:06:18 +0900

    연산자와 연산자 오버로딩

--- src/Overload.kt
+++ src/Overload.kt
@@ -4,7 +4,7 @@ fun main(args: Array<String>) {
     println(m1 + m2)
 }
 
-class Score(val a: Int, val b: Int) {
+data class Score(val a: Int, val b: Int) {
     operator fun plus(other: Score): Score {
         return Score(a + other.a, b + other.b)
 
