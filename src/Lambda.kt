dfb3b1e4b3e49c3c175b25aaf37db136f5e4a669
sklee <hadesleeq@gmail.com>
2018-06-18 13:14:20 +0900

    //익명 함수로 저으이된 함수 #2(함수 타입이 생략됨)

--- src/Lambda.kt
+++ src/Lambda.kt
@@ -8,7 +8,12 @@ fun sum2_1(a: Int, b: Int): Int = a + b
 
 fun sum2_2(a: Int, b: Int) = a + b
 
-//익명 함수로 정의된 함수 #11(함수 타입이 지정됨)
+//익명 함수로 정의된 함수 #1(함수 타입이 지정됨)
 val sum3_1: (Int, Int) -> Int = fun(x: Int, y: Int): Int { return x + y }
 val sum3_2: (Int, Int) -> Int = fun(x, y): Int { return x + y }
 val sum3_3: (Int, Int) -> Int = fun(x, y) = x + y
+
+//익명 함수로 저으이된 함수 #2(함수 타입이 생략됨)
+val sum4_1 = fun(x: Int, y: Int): Int { return x + y }
+val sum4_2 = fun(x: Int, y: Int) = x + y
+
