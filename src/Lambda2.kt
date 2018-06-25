eb0196f7d4e5728e23044c07655144e447803f51
sklee <hadesleeq@gmail.com>
2018-06-25 10:21:21 +0900

    8.2 함수타입

--- src/Lambda2.kt
+++ src/Lambda2.kt
@@ -1,12 +1,15 @@
+import java.lang.invoke.SerializedLambda
+
+fun runLambda(lambda: (Int, Int) -> Int): Int {
+    val lambdaList = ArrayList<(Int, Int) -> Int>() //#1
+    lambdaList.add(lambda) //#2
+    val a = lambdaList.get(0) //#3
+    return a(10, 20) //#4
+}
+
 fun main(args: Array<String>) {
-    val sum9: (Int, Int) -> Int = { x, y -> x + y } //#1
+    val sum9 = { x: Int, y: Int -> x + y }  //#5
 
-    val lambdaList = ArrayList<(Int, Int) -> Int>() //#2
-
-    lambdaList.add(sum9) //#3
-
-    val a: (Int, Int) -> Int = lambdaList.get(0) //#4
-
-    println("List의 람다식을 실행한 합계는 ${a(10, 20)}")
+    println("List의 람다식을 실행한 합계는 ${runLambda(sum9)}")  //#6
 
 }
